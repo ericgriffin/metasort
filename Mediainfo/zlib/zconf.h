6bc8652f4523648a3ec5d22bd1c166654881d996
eric <ericgriffin@gmail.com>
2013-06-10 20:42:25 -0400

    portability changes

--- Mediainfo/zlib/zconf.h
+++ Mediainfo/zlib/zconf.h
@@ -356,10 +356,8 @@ typedef uLong FAR uLongf;
    typedef Byte       *voidp;
 #endif
 
-#ifndef WIN32
 #if 1    /* was set to #if 1 by ./configure */
 #  define Z_HAVE_UNISTD_H
-#endif
 #endif
 
 #ifdef STDC
